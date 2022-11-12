1d47886721e2bfdf17c29d9b4f04df0ae752b738
mixnuts211 <migyeong0211@gmail.com>
2022-11-12 16:19:31 +0900

    color-flipper js 코드 작성

--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,21 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const container = document.querySelector(".container");
+const color = container.querySelector(".color");
+const btn = container.querySelector(".btn");
+
+function randomCode(){
+    return Math.floor(Math.random() * hex.length);
+}
+
+function colorSwitch(){
+    let pickColor = '#';
+    for(let i=0; i < 6; i++){
+        pickColor += hex[randomCode()];
+    }
+
+    document.body.style.backgroundColor = pickColor;
+    color.textContent = pickColor;
+}
+
+btn.addEventListener('click', colorSwitch);
